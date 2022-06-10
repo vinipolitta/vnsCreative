e470735059a2ec7c117df1b0a9c25da4e381b1a4
vinipolitta <vinicius.politta1@gmail.com>
2022-06-10 10:57:32 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'restaurant', component: RestaurantComponent },
 ];
 
 @NgModule({
