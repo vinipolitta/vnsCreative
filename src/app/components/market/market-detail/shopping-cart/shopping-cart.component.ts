47b3e8ba79561db166de96ae45011e7621cc9e44
vinipolitta <vinicius.politta1@gmail.com>
2022-06-10 17:40:48 -0300

    shopping cart

--- src/app/components/market/market-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/components/market/market-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCardService } from 'src/app/core/services/shopping-card.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCardService: ShoppingCardService) {}
+  rowState = "ready";
+  rubber = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  items(): any[] {
+    return this.shoppingCardService.items;
   }
 
+  total(): number {
+    return this.shoppingCardService.total();
+  }
+
+  clear() {
+    this.shoppingCardService.clear();
+  }
+
+  removeItem(item) {
+    console.log(item);
+
+    this.shoppingCardService.removeItem(item);
+  }
+
+  addItem(item) {
+    this.shoppingCardService.addItem(item);
+  }
 }
